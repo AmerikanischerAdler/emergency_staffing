341617614a1e1978a1640ab124b9cf1afa8cfb05
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-11-13 20:08:14 -0500

    modal now takes real data

--- static/data.js
+++ static/data.js
@@ -1,16 +1,32 @@
 let infos = document.querySelectorAll(".list-item");
-let modal = document.getElementById("modal");
+const modal = document.getElementById("modal");
+const commentText = document.getElementById("commentText");
+const commentInfo = document.getElementById("com-info");
+const closeModal = document.querySelector(".close-modal");
 
 infos.forEach((e) => {
     if (!e.classList.contains("head")) {
         e.addEventListener("click", () => {
+            const comment = e.getAttribute("data-comment");
+            const hour = e.getAttribute("data-hour");
+            const date = e.getAttribute("data-date");
+
+            let [year, month, day] = date.split("-");
+
+            commentText.textContent = comment;
+            commentInfo.textContent = `${month}/${day}/${year} - Hour ${hour}`
+
             modal.style.display = "block";
         });
     }
 });
 
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
 window.addEventListener("click", (event) => {
-    if (event.target == modal || event.target.classList.contains("close-modal")) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 });
